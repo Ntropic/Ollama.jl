61eeb1b300cc2b8d28425ddfd5f724910320c5b9
Documenter.jl <documenter@juliadocs.github.io>
2025-03-14 16:10:51 +0000

    build based on b81786b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ollama","category":"page"},{"location":"#Ollama","page":"Home","title":"Ollama","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ollama.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ollama]","category":"page"}]
+}
